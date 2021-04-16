5eacaebe6df3d556e20e92a42d4bdeb63adf8e52
Maxim Davydov <maxim.davydov.94@gmail.com>
2021-04-16 23:09:39 +0300

    Init project

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
