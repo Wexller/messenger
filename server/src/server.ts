89d7abe16dcf8c55108500ad30a5281cc38ff41f
Maxim Davydov <maxim.davydov.94@gmail.com>
2021-06-19 13:25:13 +0300

    Add router

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser';
 
+import router from './router';
+
 function loggerMiddleware(request: express.Request, response: express.Response, next) {
   console.log(`${request.method} ${request.path}`);
   next();
@@ -18,12 +20,9 @@ app.use(
     origin: process.env.CLIENT_URL,
   }),
 );
+app.use('/api', router);
 
 app.use(loggerMiddleware);
-
-app.get('/hello', (request, response) => {
-  response.json('Hello world!');
-});
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running at port ${process.env.SERVER_PORT}`);
